e83ee8c279ba91fa8b8823e4720e05cf3f85869d
yassine ELANIQ <yassaniq@gmail.com>
2025-05-02 10:12:38 +0100

    ajout d'un designe responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/core/services/recipe.service.ts
+++ src/app/core/services/recipe.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  private apiUrl = 'https://www.themealdb.com/api/json/v1/1/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRecipes(): Observable<any> {
+    return this.http.get<any>(${this.apiUrl}search.php?s=);
+  }
+
+  getMealById(id: string | null): Observable<any> {
+    return this.http.get<any>(${this.apiUrl}lookup.php?i=${id});
+  }
 }
